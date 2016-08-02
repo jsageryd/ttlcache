2d9f6c21d727d8fce506fb117b50b8570c201156
Johan Sageryd <j@1616.se>
2016-08-02 16:18:12 +0200

    Initial commit

---
+++ ttlcache.go
@@ -0,0 +1,69 @@
+// Package ttlcache provides a cache whose items expire after set TTL.
+package ttlcache
+
+import (
+	"sync"
+	"time"
+)
+
+// Cache is a key-value cache for arbitrary values
+type Cache interface {
+	Expire(key interface{})
+	Set(key interface{}, value interface{})
+	Get(key interface{}) (interface{}, bool)
+}
+
+type item struct {
+	value interface{}
+	timer *time.Timer
+}
+
+type cache struct {
+	items map[interface{}]item
+	ttl   time.Duration
+	sync.RWMutex
+}
+
+// New returns a new cache. ttl is the time to live for its items.
+func New(ttl time.Duration) Cache {
+	c := cache{
+		items: make(map[interface{}]item),
+		ttl:   ttl,
+	}
+	return &c
+}
+
+// Get retrieves a value for given key. Returned boolean is true if value
+// exists, otherwise false.
+func (c *cache) Get(key interface{}) (interface{}, bool) {
+	c.RLock()
+	defer c.RUnlock()
+	if i, ok := c.items[key]; ok {
+		return i.value, true
+	}
+	return nil, false
+}
+
+// Set updates the cache to store given value at given key.
+func (c *cache) Set(key interface{}, value interface{}) {
+	c.Lock()
+	defer c.Unlock()
+	if i, ok := c.items[key]; ok {
+		i.timer.Reset(c.ttl)
+		i.value = value
+	} else {
+		i = item{value: value, timer: time.NewTimer(c.ttl)}
+		c.items[key] = i
+		go func() {
+			<-i.timer.C
+			c.Expire(key)
+		}()
+	}
+}
+
+// Expire deletes given key.
+func (c *cache) Expire(key interface{}) {
+	c.Lock()
+	delete(c.items, key)
+	c.Unlock()
+}
---
+++ ttlcache_test.go
@@ -0,0 +1,82 @@
+package ttlcache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGetExisting(t *testing.T) {
+	c := New(1 * time.Minute)
+	key, value := "key", "value"
+	c.Set(key, value)
+
+	gotValue, ok := c.Get(key)
+
+	if !ok {
+		t.Error("ok = false, want true")
+	}
+
+	if got, want := gotValue, value; got != want {
+		t.Errorf("c.Get(%q) = %v, want %q", key, got, want)
+	}
+}
+
+func TestGetNonExistent(t *testing.T) {
+	c := New(1 * time.Minute)
+	key := "key"
+	c.Set(key, "value")
+
+	gotValue, ok := c.Get("no-key")
+
+	if ok {
+		t.Error("ok = true, want false")
+	}
+
+	if gotValue != nil {
+		t.Errorf("c.Get(%q) = %v, want nil", key, gotValue)
+	}
+}
+
+func TestGetTTL(t *testing.T) {
+	c := New(10 * time.Millisecond)
+	key, value := "key", "value"
+
+	c.Set(key, value)
+	time.Sleep(20 * time.Millisecond)
+
+	gotValue, ok := c.Get(key)
+
+	if gotValue != nil || ok {
+		t.Errorf("c.Get(%q) = %v, %v; want <nil>, false", key, gotValue, ok)
+	}
+}
+
+func TestExpire(t *testing.T) {
+	c := New(1 * time.Minute)
+	key, value := "key", "value"
+
+	c.Set(key, value)
+	c.Expire(key)
+
+	gotValue, ok := c.Get(key)
+
+	if gotValue != nil || ok {
+		t.Errorf("c.Get(%q) = %v, %v; want <nil>, false", key, gotValue, ok)
+	}
+}
+
+func TestSetTTLReset(t *testing.T) {
+	c := New(20 * time.Millisecond)
+	key, value := "key", "value"
+
+	for i := 0; i < 10; i++ {
+		c.Set(key, value)
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	gotValue, ok := c.Get(key)
+
+	if gotValue != value || !ok {
+		t.Errorf("c.Get(%q) = %v, %v; want %q, true", key, gotValue, ok, value)
+	}
+}
